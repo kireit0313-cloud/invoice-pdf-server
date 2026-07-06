ac09728d4a26e686c65a99d33a95658c8586ded6
kireit0313-cloud <kireit0313@gmail.com>
2026-07-06 15:59:14 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -366,7 +366,7 @@ app.post('/generate-pdf', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu']
     });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
